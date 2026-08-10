e416976c21aa7ce88b08bc29dfaccb264ebb7b85
bluebluesoda <300733005+bluebluesoda@users.noreply.github.com>
2026-08-10 10:44:56 +0000

    ver: bump default version to 0.2.1

--- src/internal/ver/ver.go
+++ src/internal/ver/ver.go
@@ -1,5 +1,5 @@
 package ver
 
 // Version is the vpsmgr release version. CI overrides it at build time via
-// -ldflags "-X vpsmgr/internal/ver.Version=...". Dev builds default to 0.1.0.
-var Version = "0.1.0"
+// -ldflags "-X vpsmgr/internal/ver.Version=...". Dev builds default to 0.2.1.
+var Version = "0.2.1"
